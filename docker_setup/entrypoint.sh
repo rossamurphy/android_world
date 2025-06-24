9d415bcd0ab68829f0120f248068e870841b3202
rossamurphy <ross.a.murphy@gmail.com>
2025-06-24 21:47:32 +0100

    fix timeout logic before attempting to modify settings and start app.

--- docker_setup/entrypoint.sh
+++ docker_setup/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Start Emulator
 #============================================
-export EMULATOR_TIMEOUT=600
+export EMULATOR_TIMEOUT=1000
 ./docker_setup/start_emu_headless.sh && \
 adb root && \
 python3 -m server.android_server
