39d0e656e9a7178fcee0020b9218d18858838a9d
rossamurphy <ross.a.murphy@gmail.com>
2025-06-24 20:56:11 +0100

    revert to initial.

--- docker_setup/entrypoint.sh
+++ docker_setup/entrypoint.sh
@@ -3,8 +3,5 @@
 # Start Emulator
 #============================================
 ./docker_setup/start_emu_headless.sh && \
-#x11vnc -display :99 -forever -shared -rfbport 5900 &
-#/opt/noVNC/utils/launch.sh --vnc localhost:5900 --listen 6080 &
 adb root && \
 python3 -m server.android_server
-
